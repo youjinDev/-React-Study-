267ac25ddb62134c9e16fb93eb3cc86efcafd008
YoujinCha <67622600+youjinDev@users.noreply.github.com>
2021-06-21 23:43:44 +0900

    Create useTab.js

---
+++ custom_hook_practice/useTab.js
@@ -0,0 +1,32 @@
+const data = [
+  {
+    title: "Section 1",
+    content: "내용111",
+  },
+  {
+    title: "Section 2",
+    content: "내용222",
+  },
+];
+
+const useTab = (index, data) => {
+  const [currentIndex, setCurrentIndex] = useState(index);
+  return {
+    currentTab: data[currentIndex],
+    changeItem: setCurrentIndex,
+  };
+};
+
+export default function App() {
+  const { currentTab, changeItem } = useTab(0, data);
+  return (
+    <div className="App">
+      {data.map((elem, index) => (
+        <button key={index} onClick={() => changeItem(index)}>
+          {elem.title}
+        </button>
+      ))}
+      <div>{currentTab.content}</div>
+    </div>
+  );
+}
